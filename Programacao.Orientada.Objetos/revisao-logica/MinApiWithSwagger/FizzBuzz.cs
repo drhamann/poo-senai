e1dc3446030ca73f4eabd8c477d2606572485f1d
George <george.hamann@gmail.com>
2023-11-10 22:03:42 -0300

    Resolução FizzBuzz

--- Programacao.Orientada.Objetos/revisao-logica/MinApiWithSwagger/FizzBuzz.cs
+++ Programacao.Orientada.Objetos/revisao-logica/MinApiWithSwagger/FizzBuzz.cs
@@ -11,10 +11,30 @@
             var listaFizzBuzz = new List<string>();
             for (int i = 1; i <= númeroAcalcular; i++)
             {
-                listaFizzBuzz.Add(i.ToString());
+                var fizzResult = IsFizz(i);
+                fizzResult = IsBuzz(i, fizzResult);
+                fizzResult = IsFizzBuzz(i, fizzResult);
+
+                listaFizzBuzz.Add(fizzResult);
             }
             return listaFizzBuzz.ToArray();
 
         }
+
+        private string IsFizz(int position)
+        {
+            return position % 3 == 0 ? "Fizz" : position.ToString();
+        }
+
+        private string IsBuzz(int position, string lastResult)
+        {
+            return position % 5 == 0 ? "Buzz" : lastResult;
+        }
+        private string IsFizzBuzz(int position, string lastResult)
+        {
+            return position % 3 == 0 && position % 5 == 0 ?
+                "FizzBuzz" : lastResult;
+        }
+
     }
 }
