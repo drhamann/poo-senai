96fc6e319af6af72e80c82e84572637c43e522b0
George <george.hamann@gmail.com>
2023-11-17 22:12:41 -0300

    Resolução ordenção lista

---
+++ Programacao.Orientada.Objetos/Aula-5/ExerciseSortList.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula_5
+{
+    public class ExerciseSortList
+    {
+        public int[] SortNumber(int[] numbers)
+        {
+            //TODO: Resolver.
+            /// "Resultado esperado: Se a lista for [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5], 
+            ///
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int proximoValor = numbers[j];
+                    int valorAtual = numbers[i];
+
+                    if (valorAtual > proximoValor)
+                    {
+                        numbers[i] = proximoValor;
+                        numbers[j] = valorAtual;
+                    }
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
