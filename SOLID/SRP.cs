0f3e83c74cb5091c94c511b0fd1ef618e3615734
George <george.hamann@gmail.com>
2023-11-24 20:17:43 -0300

    Remoção using não usado

--- SOLID/SRP.cs
+++ SOLID/SRP.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SOLID_SRP_VIOLANDO
+﻿namespace SOLID_SRP_VIOLANDO
 {
     class Empregado
     {
