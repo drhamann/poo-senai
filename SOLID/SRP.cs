f464d71f5c7eb4c93b06eb4e8b73d9d919154c98
George <george.hamann@gmail.com>
2023-11-24 20:17:20 -0300

    Principio da responsabilidade unica

---
+++ SOLID/SRP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_SRP_VIOLANDO
+{
+    class Empregado
+    {
+        public void SalvarNoBanco() { }
+        public void GerarRelatorio() { }
+    }
+
+}
+
+namespace SOLID_SRP_CORRETO
+{
+    class Empregado
+    {
+        public void SalvarNoBanco() { }
+    }
+    class GeradorDeRelatorio
+    {
+        public void GerarRelatorio() { }
+    }
+}
