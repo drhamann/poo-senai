2b120516e1e25d6016431155cf1f0ebb395584bc
George <george.hamann@gmail.com>
2023-11-24 20:15:45 -0300

    Principio do Aberto e fechado OCP

---
+++ SOLID/OpenClosed.cs
@@ -0,0 +1,77 @@
+﻿namespace SOLID_VIOLACAO
+{
+    public class Retangulo
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+    }
+    public class CalculadoraVolume
+    {
+        public double CalcularVolume(Retangulo retangulo, double profundidade)
+        {
+            return retangulo.Altura * retangulo.Largura * profundidade;
+        }
+    }
+
+    public class Circulo
+    {
+        public double Raio { get; set; }
+    }
+
+    public class CalculadoraArea
+    {
+        public double CalcularArea(object forma)
+        {
+            if (forma is Retangulo)
+            {
+                var retangulo = (Retangulo)forma;
+                return retangulo.Altura * retangulo.Largura;
+            }
+            else if (forma is Circulo)
+            {
+                var circulo = (Circulo)forma;
+                return Math.PI * circulo.Raio * circulo.Raio;
+            }
+            else
+            {
+                throw new ArgumentException("Tipo de forma não suportado");
+            }
+        }
+    }
+
+}
+
+namespace SOLID_CORRETO
+{
+    public abstract class Forma
+    {
+        public abstract double CalcularArea();
+    }
+
+    public class Retangulo : Forma
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+        public override double CalcularArea()
+        {
+            return Altura * Largura;
+        }
+    }
+    public class Circulo : Forma
+    {
+        public double Raio { get; set; }
+        public override double CalcularArea()
+        {
+            return Math.PI * Raio * Raio;
+        }
+    }
+
+    public class Calculadora
+    {
+        public double CalcularArea(Forma forma)
+        {
+            return forma.CalcularArea();
+        }
+    }
+
+}
--- SOLID/solid.cs
+++ SOLID/solid.cs
@@ -2,6 +2,20 @@
 {
     public class SOLID
     {
+        public void SampleExtensionOCP()
+        {
+            DateTime objetoParaTratarHora = DateTime.Now;
 
+            objetoParaTratarHora.AdicionarUmMinuto();
+        }
     }
+
+    public static class DateTimeExtension
+    {
+        public static void AdicionarUmMinuto(this DateTime dateTime)
+        {
+            dateTime.AddMinutes(1);
+        }
+    }
+
 }
